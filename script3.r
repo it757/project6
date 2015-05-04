844b3a8abefaf949b4d0379b50610caf9d334f9d
it757 <it757@yahoo.com>
2015-05-04 14:51:51 -0400

    reading excel file

--- script3.r
+++ script3.r
@@ -1,3 +1,10 @@
 mytxt2 = read.table("/mapr/pmr1/user/pfmapr1/r_test/retail_prod_FI.txt",sep="|",header=TRUE)
 mytxt2
 nrow(mytxt2)
+
+
+# Load package for Excel file and read it
+library(xlsx)
+myxlsx = read.xlsx("/mapr/pmr1/user/pfmapr1/r_test/TEST_DATA1.xlsx",1)
+system.time(read.xlsx("/mapr/pmr1/user/pfmapr1/r_test/TEST_DATA1.xlsx",1))
+nrow(myxlsx)
